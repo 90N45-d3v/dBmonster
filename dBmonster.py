ee528b8679bb674d5017dd3d93bd539cc8f71e18
90N45 <79598596+90N45-d3v@users.noreply.github.com>
2022-10-11 17:07:42 +0200

    802.11 frame tracking & Structural improvements
    New mode:
    - Advanced 802.11 frame tracking (Locate devices like Spacehuhn's Deauther by transmitted 802.11 frames)
    - Supported 802.11 frames:
    -> Deauthentication Frames
    -> Authentication Frames
    -> Probe Request Frames
    -> Beacon Frames
    
    ...and some unnecessary duplicate lines were merged into one def.

--- dBmonster.py
+++ dBmonster.py
@@ -20,6 +20,14 @@ fig = plt.gcf()
 def banner():
 	print("\033[38;5;172m" + "\n                                                   --=-+-=--\n                                                        \\\n                                                   ---=--+--=---\n                 ( ( >*< ) )                             |\\\n     _  ____          |                      _     ---=----+----=---\n    | ||  _ \\         |                     | |          |\n  __| || |_) | _ __  /_\\   ___   _ __   ___ | |_   ___  _|__\n / _` ||  _ < | '_ `'_  | / _ \\ | '_ \\ / __||  _| / _ \\|  __|\n| (_| || |_) || | | | | || (_) || | | |\\__ \\| |_ |  __/| |\n \\__,_||____/ |_| |_| |_| \\___/ |_| |_||___/ \\__| \\___||_|" + "\033[38;5;27m" + "\nby github.com/90N45-d3v" + "\033[39m")
 
+def interface_list(): # List detected interfaces
+	if platform == "linux": # Linux
+		print("\033[38;1;231m" + "\n\n  --- WiFi INTERFACES ---" + "\033[0m") # List WiFi INTERFACES
+		os.system("airmon-ng")
+	elif platform == "darwin": # MacOS
+		print("\033[38;1;231m" + "\n\n  --- WiFi INTERFACES ---" + "\033[0m\n") # List WiFi INTERFACES
+		os.system("networksetup -listallhardwareports | grep -A3 Wi-Fi | grep -A1 Device")
+
 def set_channel(): # Change channel on your WiFi card
 	if platform == "linux": # Linux
 			os.popen("iwconfig " + interface + " channel " + channel)
@@ -60,6 +68,14 @@ def sound_message():
 	print("\033[38;5;206m" + "\n [!]" + "\033[39m" + " STARTING TRACKING...")
 	print("\a\a")
 
+def graph(): # Plot figure
+	fig.set_facecolor('black') # Window bg color
+	ax.set_facecolor('black') # Graph bg color
+	ax.spines['left'].set_color('#3f64d9') # Graph axis color (left)
+	ax.spines['bottom'].set_color('#3f64d9') # Graph axis color (bottom)
+	ax.tick_params(axis='x', colors='#3f64d9') # Graph x axis text color
+	ax.tick_params(axis='y', colors='#3f64d9') # Graph y axis text color
+
 def mode1_recon(): # Scan for WiFi devices... On MacOS only Networks
 	if platform == "linux": # Linux
 		print("\n")
@@ -73,14 +89,31 @@ def mode1_recon(): # Scan for WiFi devices... On MacOS only Networks
 		exit()
 
 def mode2_update(i): # Track MAC address
-
 	if platform == "linux": # Linux
 		dBm_signal = int(os.popen("tshark -i " + interface + " -c 1 -T fields -e radiotap.dbm_antsignal ether src " + device + " 2> /dev/null | cut -d , -f 2-").read())
 	elif platform == "darwin": # MacOS
 		dBm_signal = int(os.popen("tshark -i " + interface + " -I -c 1 -T fields -e radiotap.dbm_antsignal ether src " + device + " 2> /dev/null | cut -d , -f 2-").read())
 
 	x_values.append(next(index))
+	if dBm_signal < 0: # If recieved dBm is normal, use it
+		global dBm_fallback
+		dBm_fallback = dBm_signal # Save current signal for line 50
+		y_values.append(dBm_signal)
+	elif 'dBm_fallback' in globals(): # If recieved dBm signal is broken, use last known dBm value (dBm_fallback)
+		y_values.append(dBm_fallback)
+	else: # If recieved dBm signal is broken, but there is no fallback, use -60 dBm (Can only be the first recieved signal)
+		y_values.append(-60)
+	plt.cla()
+	plt.plot(x_values, y_values, color='#ff9900')
+	plt.pause(0.003)
 
+def mode3_deauth_frames(i): # Deauthentication Frames
+	if platform == "linux":
+		dBm_signal = int(os.popen("tshark -i " + interface + " -c 1 -T fields -e radiotap.dbm_antsignal -f \"type mgt subtype deauth\" 2> /dev/null | cut -d , -f 2-").read())
+	elif platform == "darwin":
+		dBm_signal = int(os.popen("tshark -i " + interface + " -I -c 1 -T fields -e radiotap.dbm_antsignal -f \"type mgt subtype deauth\" 2> /dev/null | cut -d , -f 2-").read())
+
+	x_values.append(next(index))
 	if dBm_signal < 0: # If recieved dBm is normal, use it
 		global dBm_fallback
 		dBm_fallback = dBm_signal # Save current signal for line 50
@@ -89,20 +122,76 @@ def mode2_update(i): # Track MAC address
 		y_values.append(dBm_fallback)
 	else: # If recieved dBm signal is broken, but there is no fallback, use -65 dBm (Can only be the first recieved signal)
 		y_values.append(-60)
-
 	plt.cla()
 	plt.plot(x_values, y_values, color='#ff9900')
 	plt.pause(0.003)
 
-def mode3_file_analytics():
+def mode3_beacon_frames(i): # Beacon Frames
+	if platform == "linux":
+		dBm_signal = int(os.popen("tshark -i " + interface + " -c 1 -T fields -e radiotap.dbm_antsignal -f \"type mgt subtype beacon\" 2> /dev/null | cut -d , -f 2-").read())
+	elif platform == "darwin":
+		dBm_signal = int(os.popen("tshark -i " + interface + " -I -c 1 -T fields -e radiotap.dbm_antsignal -f \"type mgt subtype beacon\" 2> /dev/null | cut -d , -f 2-").read())
+
+	x_values.append(next(index))
+	if dBm_signal < 0: # If recieved dBm is normal, use it
+		global dBm_fallback
+		dBm_fallback = dBm_signal # Save current signal for line 50
+		y_values.append(dBm_signal)
+	elif 'dBm_fallback' in globals(): # If recieved dBm signal is broken, use last known dBm value (dBm_fallback)
+		y_values.append(dBm_fallback)
+	else: # If recieved dBm signal is broken, but there is no fallback, use -65 dBm (Can only be the first recieved signal)
+		y_values.append(-60)
+	plt.cla()
+	plt.plot(x_values, y_values, color='#ff9900')
+	plt.pause(0.003)
+
+def mode3_probe_frames(i): # Probe Request Frames
+	if platform == "linux":
+		dBm_signal = int(os.popen("tshark -i " + interface + " -c 1 -T fields -e radiotap.dbm_antsignal -f \"type mgt subtype probe-req\" 2> /dev/null | cut -d , -f 2-").read())
+	elif platform == "darwin":
+		dBm_signal = int(os.popen("tshark -i " + interface + " -I -c 1 -T fields -e radiotap.dbm_antsignal -f \"type mgt subtype probe-req\" 2> /dev/null | cut -d , -f 2-").read())
+
+	x_values.append(next(index))
+	if dBm_signal < 0: # If recieved dBm is normal, use it
+		global dBm_fallback
+		dBm_fallback = dBm_signal # Save current signal for line 50
+		y_values.append(dBm_signal)
+	elif 'dBm_fallback' in globals(): # If recieved dBm signal is broken, use last known dBm value (dBm_fallback)
+		y_values.append(dBm_fallback)
+	else: # If recieved dBm signal is broken, but there is no fallback, use -65 dBm (Can only be the first recieved signal)
+		y_values.append(-60)
+	plt.cla()
+	plt.plot(x_values, y_values, color='#ff9900')
+	plt.pause(0.003)
+
+def mode3_auth_frames(i): # Authentication Frames
+	if platform == "linux":
+		dBm_signal = int(os.popen("tshark -i " + interface + " -c 1 -T fields -e radiotap.dbm_antsignal -f \"type mgt subtype auth\" 2> /dev/null | cut -d , -f 2-").read())
+	elif platform == "darwin":
+		dBm_signal = int(os.popen("tshark -i " + interface + " -I -c 1 -T fields -e radiotap.dbm_antsignal -f \"type mgt subtype auth\" 2> /dev/null | cut -d , -f 2-").read())
+
+	x_values.append(next(index))
+	if dBm_signal < 0: # If recieved dBm is normal, use it
+		global dBm_fallback
+		dBm_fallback = dBm_signal # Save current signal for line 50
+		y_values.append(dBm_signal)
+	elif 'dBm_fallback' in globals(): # If recieved dBm signal is broken, use last known dBm value (dBm_fallback)
+		y_values.append(dBm_fallback)
+	else: # If recieved dBm signal is broken, but there is no fallback, use -65 dBm (Can only be the first recieved signal)
+		y_values.append(-60)
+	plt.cla()
+	plt.plot(x_values, y_values, color='#ff9900')
+	plt.pause(0.003)
+
+def mode4_file_analytics(): # Analyse PCAP files
 
 	print("\033[38;1;231m" + "\n\n  --- Access Points (MAC Address, SSID) ---\n" + "\033[0m")
-	os.system("tshark -r " + file + " -T fields -e wlan.sa -e wlan.ssid -Y \"wlan.fc.type_subtype == 0x0008 and !(wlan.ssid == \\\"\\\")\" | awk '!seen[$0]++'") # Filter for Beacon frames from AP's
+	os.system("tshark -r " + file + " -T fields -e wlan.sa -e wlan.ssid -Y \"wlan.fc.type_subtype == 8 and !(wlan.ssid == \\\"\\\")\" | awk '!seen[$0]++'") # Filter for Beacon frames from AP's
 
 	print("\033[38;1;231m" + "\n\n  --- Stations (MAC Address, Searching for SSID) ---\n" + "\033[0m")
-	os.system("tshark -r " + file + " -T fields -e wlan.sa -e wlan.ssid -Y \"wlan.fc.type_subtype == 0x0004 and !(wlan.ssid == \\\"\\\")\" | awk '!seen[$0]++'") # Filter for Probe Request from stations
+	os.system("tshark -r " + file + " -T fields -e wlan.sa -e wlan.ssid -Y \"wlan.fc.type_subtype == 4 and !(wlan.ssid == \\\"\\\")\" | awk '!seen[$0]++'") # Filter for Probe Request from stations
 
-def mode4_from_file(): # Track MAC address from file
+def mode5_from_file(): # Track MAC address from file
 
 	os.system("tshark -r " + file + " -T fields -e radiotap.dbm_antsignal -Y \"wlan.sa == " + device + "\" 2> /dev/null | cut -d , -f 2- > tmp_dBmonster.txt") # Filter Signals and save them to temporary file
 
@@ -118,19 +207,15 @@ def mode4_from_file(): # Track MAC address from file
 while True:
 	os.system("clear")
 	banner()
-	root_check()
 
 	if platform == "win32": # Windows
-		print("\033[48;5;9m" + " [?] WTF!?\n*** ONLY LINUX OR MAC ***" + "\033[39m")
+		print("\033[48;5;9m" + "\n [?] WTF!?\n*** ONLY LINUX OR MAC ***" + "\033[39m")
 		exit()
-	elif platform == "linux": # Linux
-		print("\033[38;1;231m" + "\n\n  --- WiFi INTERFACES ---" + "\033[0m") # List WiFi INTERFACES
-		os.system("airmon-ng")
-	elif platform == "darwin": # MacOS
-		print("\033[38;1;231m" + "\n\n  --- WiFi INTERFACES ---" + "\033[0m\n") # List WiFi INTERFACES
-		os.system("networksetup -listallhardwareports | grep -A3 Wi-Fi | grep -A1 Device")
 
-	mode = input("\033[38;1;231m" + "\n\n  --- OPTIONS ---\n\n[1]" + "\033[0m" + "\tRecon Of Wireless Landscape\n" + "\033[38;1;231m" + "[2]" + "\033[0m" + "\tRealtime MAC Address Tracking\n\n" + "\033[38;1;231m" + "[3]" + "\033[0m" + "\tPCAP File Analytics\n" + "\033[38;1;231m" + "[4]" + "\033[0m" + "\tTracking From PCAP File\n\n" + "\033[38;1;231m" + "[0]" + "\033[0m" + "\tEXIT" + "\033[38;5;172m" + "\n\n  [*]" + "\033[0m" + " Choose Option: ")
+	root_check()
+	interface_list()
+
+	mode = input("\033[38;1;231m" + "\n\n  --- OPTIONS ---\n\n[1]" + "\033[0m" + "\tRecon Of Wireless Landscape\n" + "\033[38;1;231m" + "[2]" + "\033[0m" + "\tRealtime MAC Address Tracking\n" + "\033[38;1;231m" + "[3]" + "\033[0m" + "\tAdvanced 802.11 Frame Tracking\n\n" + "\033[38;1;231m" + "[4]" + "\033[0m" + "\tPCAP File Analytics\n" + "\033[38;1;231m" + "[5]" + "\033[0m" + "\tTracking From PCAP File\n\n" + "\033[38;1;231m" + "[0]" + "\033[0m" + "\tEXIT" + "\033[38;5;172m" + "\n\n  [*]" + "\033[0m" + " Choose Option: ")
 
 	if mode == "1": # Recon
 		interface = input("\033[38;5;172m" + "\n  [*]" + "\033[39m" + " Your WiFi interface: ")
@@ -149,40 +234,67 @@ while True:
 		print("\033[38;5;206m" + "\n [!]" + "\033[39m" + " Searching for " + device + " on channel " + channel + "...")
 		sound_message()
 		fig.canvas.manager.set_window_title("dBmonster: " + device) # Window title
-		fig.set_facecolor('black') # Window bg color
-		ax.set_facecolor('black') # Graph bg color
-		ax.spines['left'].set_color('#3f64d9') # Graph axis color (left)
-		ax.spines['bottom'].set_color('#3f64d9') # Graph axis color (bottom)
-		ax.tick_params(axis='x', colors='#3f64d9') # Graph x axis text color
-		ax.tick_params(axis='y', colors='#3f64d9') # Graph y axis text color
+		graph()
 		animation = FuncAnimation(fig, mode2_update, 2000)
 		plt.show()
 		print("\033[38;1;231m" + "\nGOOD BYE!\n" + "\033[0m")
 		exit()
 
-	if mode == "3": # PCAP File Analytics
+	if mode == "3": # Advanced 802.11 Frame Tracking
+		os.system("clear")
+		banner()
+		interface_list()
+		sub_mode = input("\033[38;1;231m" + "\n\n  --- Advanced 802.11 Frame Tracking ---\n\n[1]" + "\033[0m" + "\tDeauthentication Frames\n" + "\033[38;1;231m" + "[2]" + "\033[0m" + "\tAuthentication Frames\n" + "\033[38;1;231m" + "[3]" + "\033[0m" + "\tProbe Request Frames\n" + "\033[38;1;231m" + "[4]" + "\033[0m" + "\tBeacon Frames\n\n" + "\033[38;1;231m" + "[0]" + "\033[0m" + "\tRETURN" + "\033[38;5;172m" + "\n\n  [*]" + "\033[0m" + " Choose Option: ")
+		if sub_mode == "0":
+			continue
+		interface = input("\033[38;5;172m" + "\n  [*]" + "\033[39m" + " Your WiFi interface: ")
+		channel = input("\033[38;5;172m" + "  [*]" + "\033[39m" + " WiFi channel from frame activity: ")
+		interface_check()
+		print("\033[38;5;206m" + "\n [!]" + "\033[39m" + " Setting " + interface + " to channel " + channel + "...")
+		set_channel()
+		print("\033[38;5;206m" + "\n [!]" + "\033[39m" + " Searching for specific 802.11 frame on channel " + channel + "...")
+		if sub_mode == "1":
+			fig.canvas.manager.set_window_title("dBmonster: Deauthentication Frames") # Window title
+			graph()
+			animation = FuncAnimation(fig, mode3_deauth_frames, 2000)
+			plt.show()
+
+		elif sub_mode == "2":
+			fig.canvas.manager.set_window_title("dBmonster: Authentication Frames") # Window title
+			graph()
+			animation = FuncAnimation(fig, mode3_auth_frames, 2000)
+			plt.show()
+
+		elif sub_mode == "3":
+			fig.canvas.manager.set_window_title("dBmonster: Probe Request Frames") # Window title
+			graph()
+			animation = FuncAnimation(fig, mode3_probe_frames, 2000)
+			plt.show()
+
+		elif sub_mode == "4":
+			fig.canvas.manager.set_window_title("dBmonster: Beacon Frames") # Window title
+			graph()
+			animation = FuncAnimation(fig, mode3_beacon_frames, 2000)
+			plt.show()
+		print("\033[38;1;231m" + "\nGOOD BYE!\n" + "\033[0m")
+		exit()
+
+	if mode == "4": # PCAP File Analytics
 		file = input("\033[38;5;172m" + "\n  [*]" + "\033[39m" + " Enter path to PCAP file: ")
 		print("\033[38;5;206m" + "\n [!]" + "\033[39m" + " Analyzing file " + file + "...")
-		mode3_file_analytics()
+		mode4_file_analytics()
 		input("\033[38;5;206m" + "\n [!]" + "\033[39m" + " Press the enter key to continue... (For tracking from file, remind the MAC Address your target has!)")
 		continue
 
-	if mode == "4": # Track MAC address from file
+	if mode == "5": # Track MAC address from file
 		file = input("\033[38;5;172m" + "\n  [*]" + "\033[39m" + " Enter path to PCAP file: ")
 		device = input("\033[38;5;172m" + "  [*]" + "\033[39m" + " MAC address to track: ")
 		print("\033[38;5;206m" + "\n [!]" + "\033[39m" + " Searching for " + device + " in file " + file + "...")
 		fig.canvas.manager.set_window_title("dBmonster: " + device) # Window title
 		ax.set_xlabel('Amount of recieved packets') # Graph x axis label text
 		ax.set_ylabel('Signal strength [dBm]') # Graph y axis label text
-		fig.set_facecolor('black') # Window bg color
-		ax.set_facecolor('black') # Graph bg color
-		ax.spines['left'].set_color('#3f64d9') # Graph axis color (left)
-		ax.spines['bottom'].set_color('#3f64d9') # Graph axis color (bottom)
-		ax.tick_params(axis='x', colors='#3f64d9') # Graph x axis text color
-		ax.tick_params(axis='y', colors='#3f64d9') # Graph y axis text color
-		ax.xaxis.label.set_color('#3f64d9') # Graph x axis label color
-		ax.yaxis.label.set_color('#3f64d9') # Graph y axis label color
-		mode4_from_file()
+		graph()
+		mode5_from_file()
 		plt.show()
 		if os.path.exists("tmp_dBmonster.txt"): # If it exists, delete temporary file
 			os.remove("tmp_dBmonster.txt")
